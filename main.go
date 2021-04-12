1489829cba63bd45fa5317a2730c3d5d9bdff945
xingwei <xingwei@optaim.com>
2021-04-12 10:48:43 +0800

    set header

--- main.go
+++ main.go
@@ -6,9 +6,19 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
-    fmt.Fprint(w, "请求的路径为：" + r.URL.Path)
+    w.Header().Set("Content-Type","text/html; charset=utf-8")
+    switch r.URL.Path {
+        case "/":
+            fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+        case "/home":
+            fmt.Fprint(w, "<h1>Hello, 这里是 home</h1>")
+        case "/me":
+            fmt.Fprint(w, "<h1>Hello, I‘m Sreio。</h1>")
+        default:
+            fmt.Fprint(w, "默认跳转路由")
+    }
 }
+
 func main() {
     http.HandleFunc("/", handlerFunc)
     http.ListenAndServe(":3000", nil)
