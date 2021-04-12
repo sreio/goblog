5314f5350adc4bbbf8a4ec8f29473add1a7938bf
xingwei <xingwei@optaim.com>
2021-04-12 15:46:53 +0800

    新增表单

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module goblog
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1
+)
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var router = mux.NewRouter()
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
 }
@@ -36,6 +38,26 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "创建新的文章")
 }
 
+func articlesAddHandler(w http.ResponseWriter, r *http.Request) {
+    html := `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>创建文章 —— 我的技术博客</title>
+    </head>
+    <body>
+        <form action="%s" method="post">
+            <p><input type="text" name="title"></p>
+            <p><textarea name="body" cols="30" rows="10"></textarea></p>
+            <p><button type="submit">提交</button></p>
+        </form>
+    </body>
+    </html>
+    `
+    storeURL, _ := router.Get("articles.store").URL()
+    fmt.Fprintf(w, html, storeURL)
+}
+
 
 func foreaHtmlMiddlewaer (next http.Handler) http.Handler {
     return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request)  {
@@ -54,14 +76,13 @@ func removeTrailingSlash (next http.Handler) http.Handler {
 }
 
 func main() {
-    router := mux.NewRouter().StrictSlash(true)
-
     router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
     router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
     router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
     router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
     router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+    router.HandleFunc("/articles/add", articlesAddHandler).Methods("GET").Name("articles.add")
 
     // 自定义 404 页面
     router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
@@ -69,10 +90,10 @@ func main() {
     router.Use(foreaHtmlMiddlewaer)
 
     // 通过命名路由获取 URL 示例
-    homeURL, _ := router.Get("home").URL()
-    fmt.Println("homeURL: ", homeURL)
-    articleURL, _ := router.Get("articles.show").URL("id", "23")
-    fmt.Println("articleURL: ", articleURL)
+    // homeURL, _ := router.Get("home").URL()
+    // fmt.Println("homeURL: ", homeURL)
+    // articleURL, _ := router.Get("articles.show").URL("id", "23")
+    // fmt.Println("articleURL: ", articleURL)
 
     http.ListenAndServe(":3000", removeTrailingSlash(router))
 }
